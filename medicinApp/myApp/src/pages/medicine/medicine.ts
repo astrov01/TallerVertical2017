461163602a08ea0fdd65a0ed92f1e1abf8811c5f
Victor Espinoza <vic.espinozac@gmail.com>
2017-03-04 14:55:13 -0700

    hola

---
+++ medicinApp/myApp/src/pages/medicine/medicine.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-medicine',
+  templateUrl: 'medicine.html'
+})
+export class MedicinePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
